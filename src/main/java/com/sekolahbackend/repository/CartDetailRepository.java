0739536a4206cd9b26fd8e6f3d4d8a2c4dc3af68
Aries Prayoga <ariezprayoga@gmail.com>
2020-03-25 07:20:25 +0700

    add cart

--- src/main/java/com/sekolahbackend/repository/CartDetailRepository.java
+++ src/main/java/com/sekolahbackend/repository/CartDetailRepository.java
@@ -1,9 +1,15 @@
 package com.sekolahbackend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sekolahbackend.entity.CartDetail;
+import com.sekolahbackend.entity.CartDetail.CartDetailStatus;
 
 public interface CartDetailRepository extends JpaRepository<CartDetail, Integer> {
 
+	@Query("FROM CartDetail detail WHERE detail.cart.user.id = ?1 AND detail.book.id = ?2 AND detail.cartDetailStatus = ?3")
+	List<CartDetail> findByUserIdAndBookIdAndDetailStatus(Integer userId, Integer bookId, CartDetailStatus status);
 }
