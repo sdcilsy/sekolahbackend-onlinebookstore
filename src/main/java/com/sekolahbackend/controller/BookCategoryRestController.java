a38114ba3bfdb15705755c5081508d1418a253e6
Aries Prayoga <ariezprayoga@gmail.com>
2020-03-29 08:51:03 +0700

    add preauthorize for endpoints in book category

--- src/main/java/com/sekolahbackend/controller/BookCategoryRestController.java
+++ src/main/java/com/sekolahbackend/controller/BookCategoryRestController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,7 @@ public class BookCategoryRestController {
 	@Autowired
 	private BookCategoryService bookCategoryService;
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping("/save")
 	public BookCategoryModel save(@RequestBody @Valid BookCategoryRequestCreateModel request, 
 			BindingResult result,
@@ -47,6 +49,7 @@ public class BookCategoryRestController {
 		}
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping("/update")
 	public BookCategoryModel update(@RequestBody @Valid BookCategoryRequestUpdateModel request, 
 			BindingResult result,
@@ -61,16 +64,19 @@ public class BookCategoryRestController {
 		}
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping("/deleteById/{id}")
 	public BookCategoryModel delete(@PathVariable("id") final Integer id) {
 		return bookCategoryService.deleteById(id);
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT', 'ROLE_USER')")
 	@GetMapping("/findAll")
 	public List<BookCategoryModel> findAll() {
 		return bookCategoryService.findAll();
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT', 'ROLE_USER')")
 	@GetMapping("/findById/{id}")
 	public BookCategoryModel findById(@PathVariable("id") final Integer id) {
 		return bookCategoryService.findById(id);
