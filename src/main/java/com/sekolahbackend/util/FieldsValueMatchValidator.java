f0ed53d06de15c62e8b886d787d264978c884ccf
Aries Prayoga <ariezprayoga@gmail.com>
2020-03-29 08:49:20 +0700

    fix indentation

--- src/main/java/com/sekolahbackend/util/FieldsValueMatchValidator.java
+++ src/main/java/com/sekolahbackend/util/FieldsValueMatchValidator.java
@@ -7,24 +7,24 @@ import org.springframework.beans.BeanWrapperImpl;
 
 public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValueMatch, Object> {
 
-	 private String field;
-	    private String fieldMatch;
+	private String field;
+	private String fieldMatch;
 
-	    public void initialize(FieldsValueMatch constraintAnnotation) {
-	        this.field = constraintAnnotation.field();
-	        this.fieldMatch = constraintAnnotation.fieldMatch();
-	    }
+	public void initialize(FieldsValueMatch constraintAnnotation) {
+		this.field = constraintAnnotation.field();
+		this.fieldMatch = constraintAnnotation.fieldMatch();
+	}
 
-	    public boolean isValid(Object value, ConstraintValidatorContext context) {
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
 
-	        Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
-	        Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
-	        
-	        if (fieldValue != null) {
-	            return fieldValue.equals(fieldMatchValue);
-	        } else {
-	            return fieldMatchValue == null;
-	        }
+		Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
+		Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
 
-	    }
+		if (fieldValue != null) {
+			return fieldValue.equals(fieldMatchValue);
+		} else {
+			return fieldMatchValue == null;
+		}
+
+	}
 }
