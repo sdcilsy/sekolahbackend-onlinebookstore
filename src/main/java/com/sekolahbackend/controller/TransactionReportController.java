7935ab50705f7baaeaaaf242437af3bda489cc41
Aries Prayoga <ariezprayoga@gmail.com>
2020-04-04 13:55:43 +0700

    fix authorization

--- src/main/java/com/sekolahbackend/controller/TransactionReportController.java
+++ src/main/java/com/sekolahbackend/controller/TransactionReportController.java
@@ -5,6 +5,7 @@ import static com.sekolahbackend.util.EndpointConstant.PER_PAGE;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,7 @@ public class TransactionReportController {
 	@Autowired
 	private TransactionService transactionService;
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@GetMapping("/load")
 	public Page<TransactionModel> load(
 			@RequestParam(value = PAGE, required = false) Integer page,
