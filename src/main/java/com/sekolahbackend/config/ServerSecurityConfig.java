424c3ba20380c1428880a072ac4d51af5bcd0e21
Aries Prayoga <ariezprayoga@gmail.com>
2020-05-28 13:06:35 +0700

    fix cors

--- src/main/java/com/sekolahbackend/config/ServerSecurityConfig.java
+++ src/main/java/com/sekolahbackend/config/ServerSecurityConfig.java
@@ -1,16 +1,26 @@
 package com.sekolahbackend.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.sekolahbackend.service.UserService;
 
@@ -48,4 +58,36 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-ui.html",
                 "/webjars/**");
     }
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests().antMatchers("/api/rest**").authenticated()
+			.and()
+			.authorizeRequests().antMatchers("/oauth/token").authenticated()
+			.and()
+			.authorizeRequests()
+				.anyRequest().permitAll()
+				.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.anonymous().disable()
+			.cors();
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilterBean() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("*"));
+		configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+		corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return corsFilter;
+	}
 }
